5dfc0c8fb45f211c60a172118bccce7097011fb9
Siwon-Kim <siwonkim1108@gmail.com>
2023-04-21 02:33:52 -0400

    docs: minor changes in swagger.js #4

--- swagger.js
+++ swagger.js
@@ -3,7 +3,7 @@ const swaggerAutogen = require("swagger-autogen")();
 const doc = {
 	info: {
 		title: "항해 14기 노드반 HW3 Blog APIs",
-		description: "김시원, 정하욱",
+		description: "김시원, 정하욱 - sequelize",
 	},
 	host: "localhost:3000",
 	schemes: ["http"],
