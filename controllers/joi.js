ba2f87048f331a3ad4c78bdbc45eedf04c204107
Siwon-Kim <siwonkim1108@gmail.com>
2023-04-27 03:42:11 -0400

    refactor: comments & users API 3-layered arch. #17 #18 #22

---
+++ controllers/joi.js
@@ -0,0 +1,64 @@
+const Joi = require("joi");
+
+module.exports = {
+	signupSchema: Joi.object({
+		nickname: Joi.string()
+			.regex(/^[a-zA-Z0-9]{3,}$/)
+			.messages({
+				"string.base": "닉네임의 형식이 일치하지 않습니다.",
+				"string.pattern.base": "닉네임의 형식이 일치하지 않습니다.",
+				"string.empty": "닉네임의 형식이 일치하지 않습니다.",
+				"string.min": "닉네임의 형식이 일치하지 않습니다.",
+			}),
+		password: Joi.string().min(4).required().messages({
+			"string.base": "패스워드의 형식이 일치하지 않습니다.",
+			"string.empty": "패스워드 형식이 일치하지 않습니다.",
+			"string.min": "패스워드 형식이 일치하지 않습니다.",
+		}),
+		confirmedPassword: Joi.string().valid(Joi.ref("password")).required().messages({
+			"string.base": "패스워드가 일치하지 않습니다.",
+			"any.only": "패스워드가 일치하지 않습니다.",
+			"string.empty": "패스워드가 일치하지 않습니다.",
+		}),
+	})
+		.custom((value, helpers) => {
+			if (value.password.toLowerCase().includes(value.nickname.toLowerCase())) {
+				return helpers.error("custom.passwordContainsNickname");
+			}
+			return value;
+		})
+		.message({
+			"custom.passwordContainsNickname": "패스워드에 닉네임이 포함되어 있습니다.",
+		}),
+	loginSchema: Joi.object({
+		nickname: Joi.string().required().min(1).messages({
+			"string.base": "닉네임 또는 패스워드를 확인해주세요.",
+			"string.empty": "닉네임 또는 패스워드를 확인해주세요.",
+			"string.min": "닉네임 또는 패스워드를 확인해주세요.",
+		}),
+		password: Joi.string().required().messages({
+			"string.base": "닉네임 또는 패스워드를 확인해주세요.",
+			"string.empty": "닉네임 또는 패스워드를 확인해주세요.",
+			"string.min": "닉네임 또는 패스워드를 확인해주세요.",
+		}),
+	}),
+	commentSchema: Joi.object({
+		comment: Joi.string().min(1).required().messages({
+			"string.base": "데이터 형식이 올바르지 않습니다.",
+			"string.empty": "댓글 내용을 입력해주세요.",
+			"string.min": "데이터 형식이 올바르지 않습니다.",
+		}),
+	}),
+	postSchema: Joi.object({
+		title: Joi.string().min(1).required().messages({
+			"string.base": "게시글 제목의 형식이 일치하지 않습니다.",
+			"string.empty": "게시글 제목을 입력해주세요.",
+			"string.min": "게시글 제목의 형식이 일치하지 않습니다.",
+		}),
+		content: Joi.string().min(1).required().messages({
+			"string.base": "게시글 내용의 형식이 일치하지 않습니다.",
+			"string.empty": "게시글 내용을 입력해주세요.",
+			"string.min": "게시글 내용의 형식이 일치하지 않습니다.",
+		}),
+	}),
+};
--- controllers/users.controller.js
+++ controllers/users.controller.js
@@ -1,75 +1,48 @@
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcrypt");
-const salt = 12;
-const SECRET_KEY = "secretkeyforvoyageblog";
 const UserService = require("../services/users.service");
+const { signupSchema, loginSchema } = require("./joi");
+require("dotenv").config();
 
 class UserController {
 	userService = new UserService();
 
 	// POST: 회원 가입 API
 	signup = async (req, res, next) => {
-		const { nickname, password, confirmedPassword } = req.body;
-
-		if (typeof nickname !== "string")
-			throw new Error("412/닉네임의 형식이 일치하지 않습니다.");
-
-		if (password !== confirmedPassword)
-			throw new Error("412/패스워드가 일치하지 않습니다.");
-
-		if (password.length < 4 || typeof password !== "string")
-			throw new Error("412/패스워드 형식이 일치하지 않습니다.");
-
-		if (password.includes(nickname.toLowerCase()))
-			throw new Error("412/패스워드에 닉네임이 포함되어 있습니다.");
-
-		const nickNameRegex = new RegExp("^[a-zA-z0-9]{3,}$", "g");
-		if (!nickNameRegex.test(nickname))
-			throw new Error("412/닉네임의 형식이 일치하지 않습니다.");
-
 		try {
-			const existingUser = await this.userService.findUser(nickname);
-			if (existingUser) throw new Error("412/중복된 닉네임입니다.");
+			const { nickname, password } = await signupSchema
+				.validateAsync(req.body)
+				.catch((error) => {
+					throw new Error(`412/${error}`);
+				});
 
-			const hashedPassword = await bcrypt.hash(password, salt);
-			await this.userService.createAccount(nickname, hashedPassword);
+			await this.userService.createAccount(nickname, password);
 			res.status(201).json({ message: "회원 가입에 성공하였습니다." });
 		} catch (error) {
-			throw new Error(
-				error.message || "400/요청한 데이터 형식이 올바르지 않습니다."
-			);
+			console.error(`${req.method} ${req.originalUrl} : ${error.message}`);
+			throw new Error(error.message || "400/요청한 데이터 형식이 올바르지 않습니다.");
 		}
 	};
 
 	// POST: 로그인 API
 	login = async (req, res, next) => {
-		const { nickname, password } = req.body;
-
-		if (typeof nickname !== "string" || typeof password !== "string")
-			throw new Error("412/닉네임 또는 패스워드를 확인해주세요.");
-
 		try {
-			const existingUser = await this.userService.findUser(nickname);
-			let validInput = false;
-			if (existingUser) {
-				const hashedPassword = existingUser.password;
-				const matchingPassword = await bcrypt.compare(
-					password,
-					hashedPassword
-				);
-				if (matchingPassword) validInput = true;
-			}
+			const { nickname, password } = await loginSchema
+				.validateAsync(req.body)
+				.catch((error) => {
+					throw new Error(`412/${error}`);
+				});
 
-			if (!validInput)
-				throw new Error("412/닉네임 또는 패스워드를 확인해주세요.");
-
-			const token = jwt.sign({ userId: existingUser.userId }, SECRET_KEY);
-			res.cookie("Authorization", `Bearer ${token}`);
-			return res.status(200).json({ token });
+			const { accessToken, refreshToken } = await this.userService.login(
+				nickname,
+				password
+			);
+			res.cookie("accessToken", `Bearer ${accessToken}`);
+			res.cookie("refreshToken", `Bearer ${refreshToken}`);
+			return res.status(200).json({ message: "Token이 정상적으로 발급되었습니다." });
 		} catch (error) {
+			console.error(`${req.method} ${req.originalUrl} : ${error.message}`);
 			throw new Error(error.message || "400/로그인에 실패하였습니다.");
 		}
 	};
 }
 
-module.exports = UserController;
+module.exports = { UserController };
